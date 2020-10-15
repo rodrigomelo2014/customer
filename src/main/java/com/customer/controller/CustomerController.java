fcdf3032478196840db016d2e8cc256e14255c29
Rodrigo Melo <romelo@uolinc.com>
2020-10-15 11:17:20 -0300

    descricao dos endpoints no swagger

--- src/main/java/com/customer/controller/CustomerController.java
+++ src/main/java/com/customer/controller/CustomerController.java
@@ -1,13 +1,15 @@
 package com.customer.controller;
 
 import java.util.List;
-
 import javax.validation.Valid;
 
 import com.customer.controller.dto.Customer;
 import com.customer.exceptions.CustomerNotFoundException;
 import com.customer.service.CustomerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,32 +27,61 @@ public class CustomerController {
 
     @Autowired private CustomerService customerService;
 
+    @Operation(summary = "List all customers registered")
     @GetMapping
     public ResponseEntity<List<Customer>> findAllCustomers() {
         return ResponseEntity.ok(customerService.findAll());
     }
 
-    @GetMapping("/{id}")
-    public ResponseEntity<Customer> findCustomerById(@PathVariable Long id) throws CustomerNotFoundException {
+    @Operation(summary = "Find a customer by id")
+    @GetMapping(
+            value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> findCustomerById(@PathVariable Long id)
+            throws CustomerNotFoundException {
         return ResponseEntity.ok(customerService.findCustomer(id));
     }
 
-    @PostMapping
-    public ResponseEntity<Customer> createCustomer(@Valid @RequestBody Customer request) {
+    @Operation(summary = "Create a customer")
+    @PostMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> createCustomer(
+            @Valid @Parameter(description = "Customer to be created", required = true) @RequestBody
+                    Customer request) {
         return ResponseEntity.ok(customerService.create(request));
     }
 
-    @PatchMapping
-    public ResponseEntity<Customer> updateCustomer(@Valid @RequestBody Customer request) {
+    @Operation(summary = "Update a customer")
+    @PatchMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> updateCustomer(
+            @Valid
+                    @Parameter(description = "Customer to be fully updated", required = true)
+                    @RequestBody
+                    Customer request) {
         return ResponseEntity.ok(new Customer());
     }
 
-    @PutMapping
-    public ResponseEntity<Customer> partialUpdateCustomer(@Valid @RequestBody Customer request) {
+    @Operation(summary = "Partial update a customer")
+    @PutMapping(
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Customer> partialUpdateCustomer(
+            @Valid
+                    @Parameter(description = "Customer to be partially updated", required = true)
+                    @RequestBody
+                    Customer request) {
         return ResponseEntity.ok(new Customer());
     }
 
-    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a customer")
+    @DeleteMapping(
+            value = "/{id}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Customer> deleteCustomer(@PathVariable Long id) {
         return ResponseEntity.ok(new Customer());
     }
